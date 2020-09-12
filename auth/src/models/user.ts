07c36766b89c41c01e8ae03293afc9c69584ac8c
zhenyu zhong <genericname001@gmail.com>
2020-09-12 16:14:26 -0500

    add in user creation and pw hashing

---
+++ auth/src/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request.error";
 
 const router = express.Router();
 
@@ -22,9 +23,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError("Email already exists");
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
