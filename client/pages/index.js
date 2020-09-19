d876609177f631cdb368b6aaba193e365d447496
zhenyu zhong <genericname001@gmail.com>
2020-09-19 14:19:17 -0500

    some ui

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/curentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
