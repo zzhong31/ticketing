db2c2bcd042133eec1f1eb431f0edd6cad21c1b2
zhenyu zhong <genericname001@gmail.com>
2020-09-19 19:14:08 -0500

    move some functions to common npm

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -14,7 +14,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get('/api/users/currentuser');
-  console.log(appContext.Component);
+
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
     pageProps = await appContext.Component.getInitialProps(appContext.ctx);
